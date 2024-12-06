12789b78aed294a55cf5ca5a6653bba6d21adc6b
userquin <userquin@gmail.com>
2024-12-06 17:42:09 +0100

    Initial commit

---
+++ src/runtime/plugin.ts
@@ -0,0 +1,8 @@
+import { defineNuxtPlugin } from '#imports'
+import type {} from '#app'
+
+/*const plugin: Plugin =*/ export default defineNuxtPlugin((_nuxtApp) => {
+  console.log('Plugin injected by my-module!')
+})
+
+// export default plugin
